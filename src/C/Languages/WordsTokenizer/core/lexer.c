d512343567684eeecde29b92292719d72f9daa60
JoshuaLight <ashkariel.eter@gmail.com>
2019-10-20 15:48:03 +0300

    Added `lexer` to `CMakeLists`.

--- src/C/Languages/WordsTokenizer/core/lexer.c
+++ src/C/Languages/WordsTokenizer/core/lexer.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
 #include "lexer.h"
+
+Tokens* parse_tokens(char *source) {
+    puts("Parsing tokens");
+
+    return 0;
+}
--- src/C/Languages/WordsTokenizer/main.c
+++ src/C/Languages/WordsTokenizer/main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include "common/cursor.h"
+#include "core/lexer.h"
 
 int main(int args_count, char** args) {
-    StringCursor cursor = cursor_of("Wello");
-
-    /*
-     * 1. Parse tokens from text.
-     * 1.1. Create cursor on source.
-     * 1.2. Read next token while they're available.
-     * 1.3. Create a linked list of tokens.
-     * 2. Show them.
-     */
+    Tokens* tokens = parse_tokens("A");
 
     return 0;
 }
