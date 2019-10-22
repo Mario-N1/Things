b25e36301eec1670dfc079a2b956afc580481036
JoshuaLight <ashkariel.eter@gmail.com>
2019-10-22 13:19:08 +0300

    Removed `cursor`.

--- src/C/Languages/WordsTokenizer/common/cursor.c
+++
@@ -1,9 +0,0 @@
-#include "cursor.h"
-
-StringCursor inline cursor_of(char *source) {
-    return (StringCursor) { source, 0 };
-}
-
-char inline current_of(StringCursor cursor) {
-    return *(cursor.source + cursor.index);
-}
