5982ff3fc71228a532079139f0f20fe1836d44d2
JoshuaLight <ashkariel.eter@gmail.com>
2019-10-28 17:01:31 +0200

    Improved `vs` funciton definition.

--- src/Haskell/Games/RockPaperScissors/src/Game.hs
+++ src/Haskell/Games/RockPaperScissors/src/Game.hs
@@ -14,12 +14,12 @@ weaponOf :: String -> Weapon
 weaponOf = read
 
 vs :: Weapon -> Weapon -> Result
-vs Scissors Paper    = Win
-vs Scissors Rock     = Lose
-vs Scissors Scissors = Draw
-vs Paper    Rock     = Win
-vs Paper    Scissors = Lose
-vs Paper    Paper    = Draw
-vs Rock     Scissors = Win
-vs Rock     Paper    = Lose
-vs Rock     Rock     = Draw
+Scissors `vs` Paper    = Win
+Scissors `vs` Rock     = Lose
+Scissors `vs` Scissors = Draw
+Paper    `vs` Rock     = Win
+Paper    `vs` Scissors = Lose
+Paper    `vs` Paper    = Draw
+Rock     `vs` Rock     = Win
+Rock     `vs` Scissors = Lose
+Rock     `vs` Paper    = Draw
