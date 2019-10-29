946e793f3555739f4efd82796ef839350041c1be
JoshuaLight <ashkariel.eter@gmail.com>
2019-10-29 12:18:09 +0200

    Added infinite game rounds to Rock-paper-scissors.

--- src/Haskell/Games/RockPaperScissors/src/Main.hs
+++ src/Haskell/Games/RockPaperScissors/src/Main.hs
@@ -3,6 +3,19 @@ module Main where
 import System.IO
 import Game
 
+data Answer = Yes | No
+
+instance Read Answer where
+  readsPrec _ "y" = [(Yes, "")]
+  readsPrec _ "n" = [(No, "")]
+  readsPrec _ _ = []
+
+ask :: String -> IO Answer
+ask question = do
+  putStr question
+  char <- getLine
+  return $ read char
+
 ioWeapon :: IO Weapon
 ioWeapon = do
   putStr "Pick weapon: "
@@ -18,7 +31,13 @@ loop = do
   ai     <- ioWeapon
 
   putStrLn $ show $ player `vs` ai
-  end
+
+  answer <- ask "Wanna repeat? "
+  continue answer
+
+continue :: Answer -> IO ()
+continue Yes = loop
+continue No  = end
 
 main :: IO ()
 main = do
