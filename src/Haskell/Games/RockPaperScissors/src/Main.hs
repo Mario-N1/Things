87d1439c905e453a7e06c60f79784486307df98e
JoshuaLight <ashkariel.eter@gmail.com>
2019-11-04 14:17:38 +0200

    Changed Rock-paper-scissors so AI is using random weapon now.

--- src/Haskell/Games/RockPaperScissors/src/Main.hs
+++ src/Haskell/Games/RockPaperScissors/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.IO
+import System.Random
 import Game
 import Beauty
 
@@ -10,6 +11,22 @@ instance Read Answer where
   readsPrec _ "y" = [(Yes, "")]
   readsPrec _ "n" = [(No, "")]
   readsPrec _  _  = []
+
+randomInt :: Int -> Int -> IO Int
+randomInt from to = do
+  g <- getStdGen
+  let (x, _) = randomR (from, to) g
+  return x
+
+randomWeapon :: IO Weapon
+randomWeapon = do
+  x <- randomInt 1 3
+  return (x |> asWeapon)
+
+asWeapon :: Int -> Weapon
+asWeapon 1 = Rock
+asWeapon 2 = Paper
+asWeapon 3 = Scissors
 
 ask :: (Read a) => String -> IO a
 ask question = do
@@ -21,7 +38,7 @@ ask question = do
 loop :: IO ()
 loop = do
   player <- ask "Pick weapon: "
-  ai     <- ask "Pick weapon: "
+  ai     <- randomWeapon
 
   player `vs` ai
     |> asText
